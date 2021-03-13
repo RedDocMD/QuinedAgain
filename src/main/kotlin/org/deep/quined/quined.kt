715831a3294745d4d2ab6bca24301ef20b87d055
Deep Majumder <deep.majumder2019@gmail.com>
2021-03-13 21:00:13 +0530

    Added cube joining

--- src/main/kotlin/org/deep/quined/quined.kt
+++ src/main/kotlin/org/deep/quined/quined.kt
@@ -5,18 +5,37 @@ import java.lang.RuntimeException
 enum class Term {
     Zero, One, DontCare;
 
-    override fun toString(): String {
-        return when (this) {
+    override fun toString() =
+        when (this) {
             Zero -> "0"
             One -> "1"
             DontCare -> "-"
         }
-    }
 }
 
 class Cube(val terms: List<Term>, val minTerms: List<Int>) {
     constructor(minTerm: Int, termCount: Int) : this(minTermToTerm(minTerm, termCount), listOf(minTerm))
     constructor(terms: List<Term>) : this(terms, termsToMinTerms(terms))
+}
+
+fun canJoin(cube1: Cube, cube2: Cube): Boolean {
+    if (cube1.terms.size != cube2.terms.size)
+        return false;
+    var diffFoundYet = false
+    for ((term1, term2) in cube1.terms.zip(cube2.terms)) {
+        if (term1 != term2) {
+            if (!diffFoundYet) diffFoundYet = false
+            else return false;
+        }
+    }
+    return diffFoundYet
+}
+
+// Precondition: canJoin cube1 and cube2
+fun joinCube(cube1: Cube, cube2: Cube): Cube {
+    val joinedTerms =
+        cube1.terms.zip(cube2.terms).map { (term1, term2) -> if (term1 == term2) term2 else Term.DontCare }
+    return Cube(joinedTerms)
 }
 
 fun minTermToTerm(minTerm: Int, termCount: Int): List<Term> {
@@ -27,7 +46,7 @@ fun minTermToTerm(minTerm: Int, termCount: Int): List<Term> {
         terms.add(if (rem == 0) Term.Zero else Term.One)
         remaining /= 2
     }
-    val padding =  List(termCount - terms.size) {Term.Zero}
+    val padding = List(termCount - terms.size) { Term.Zero }
     terms.addAll(padding)
     terms.reverse()
     return terms
@@ -35,7 +54,7 @@ fun minTermToTerm(minTerm: Int, termCount: Int): List<Term> {
 
 
 fun termsToMinTerms(terms: List<Term>): MutableList<Int> {
-    if (!terms.contains(Term.DontCare)) {
+    if (Term.DontCare !in terms) {
         return mutableListOf(termsToInt(terms))
     }
     val dontCareIdx = terms.indexOf(Term.DontCare)
