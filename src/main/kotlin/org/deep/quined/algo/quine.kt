7f6542ab33b019bf2ae25ab91df3f232124f3823
Deep Majumder <deep.majumder2019@gmail.com>
2021-03-13 22:56:14 +0530

    Single step reduction

--- src/main/kotlin/org/deep/quined/algo/quine.kt
+++ src/main/kotlin/org/deep/quined/algo/quine.kt
@@ -9,3 +9,33 @@ fun partitionCubesByOneCount(cubes: List<Cube>): Map<Int, List<Cube>> {
 }
 
 data class ReductionStepResult(val newCubes: List<Cube>, val oldPrimes: List<Cube>)
+
+fun reduceOneStep(partitionedCubes: Map<Int, List<Cube>>, termCount: Int): ReductionStepResult {
+    val newCubes = mutableListOf<Cube>()
+    val cubesCovered = mutableMapOf<Cube, Boolean>()
+    for ((_, cubes) in partitionedCubes) {
+        for (cube in cubes) {
+            cubesCovered[cube] = false
+        }
+    }
+    for (i in 1 until termCount) {
+        val lowerList = partitionedCubes[i]
+        val higherList = partitionedCubes[i + 1]
+        if (lowerList != null && higherList != null) {
+            for (lowerCube in lowerList) {
+                for (higherCube in higherList) {
+                    if (lowerCube.canJoin(higherCube)) {
+                        newCubes.add(lowerCube.joinCube(higherCube))
+                        cubesCovered[lowerCube] = true
+                        cubesCovered[higherCube] = true
+                    }
+                }
+            }
+        }
+    }
+    val cubesNotCovered = mutableListOf<Cube>()
+    for ((cube, covered) in cubesCovered) {
+        if (!covered) cubesNotCovered.add(cube)
+    }
+    return ReductionStepResult(newCubes, cubesNotCovered)
+}
